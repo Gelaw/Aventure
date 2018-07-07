af6b88ab8c03fc5108d7f9eef9d124d5753f5930
Gelaw <Gelaw974@gmail.com>
2018-07-07 18:02:28 +0200

    updated room for love 0.10.2

--- room.lua
+++ room.lua
@@ -5,14 +5,14 @@ room.tilesize = 0
 function room.init()
   room.ground = {
   {1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,1},
   {1,1,1,1,1,1,1,1,1,1}}
   room.tilesize = 50
 end
@@ -25,12 +25,12 @@ function room.draw(xHero, yHero)
       if x <= 0 or x > #room.ground[1] or y <= 0 or y > #room.ground then
         love.graphics.setColor(0, 0, 0)
       elseif room.ground[x][y] == 0 then
-        love.graphics.setColor(0, 1, 0)
+        love.graphics.setColor(0, 255, 0)
       elseif room.ground[x][y] == 1 then
-        love.graphics.setColor(1, 0, 0)
+        love.graphics.setColor(255, 0, 0)
       end
-      love.graphics.rectangle("fill", x * room.tilesize, y * room.tilesize, room.tilesize, room.tilesize)
-      love.graphics.setColor(1, 1, 1)
+      love.graphics.rectangle("fill", (x - xHero )* room.tilesize, (y - yHero) * room.tilesize, room.tilesize, room.tilesize)
+      love.graphics.setColor(255, 255, 255)
     end
   end
 end
