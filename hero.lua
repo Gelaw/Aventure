c48676a24298cf6f615496cb52af7692ef337f84
Sungie <benmanu974@gmail.com>
2018-07-07 17:16:35 +0200

    modif hero position

--- hero.lua
+++ hero.lua
@@ -3,35 +3,41 @@ local hero = {}
 hero.x = 0
 hero.y = 0
 
+
 hero.width = 100
 hero.height = 100
 hero.speed = 10
 hero.offSetX = hero.width/2
 hero.offSetY = hero.height/2
 
+hero.screenX = hero.width * hero.x
+hero.screenY = hero.height * hero.y
+
 function hero.init()
-  hero.x = love.graphics.getWidth()/2
-  hero.y = love.graphics.getHeight()/2
+  hero.x = 5
+  hero.y = 5
+  --hero.screenX = love.graphics.getWidth()/2
+  --hero.screenY = love.graphics.getHeight()/2
 end
 
 function hero.update(dt)
 
-    if love.keyboard.isDown("z") and hero.x - hero.offSetX > 0 then
-      hero.y = hero.y - hero.speed
+    if love.keyboard.isDown("z") and hero.screenY - hero.offSetY > 0 then
+      hero.screenY = hero.screenY - hero.speed
     end
-    if love.keyboard.isDown("s") and hero.x + hero.offSetX < width then
-      hero.y = hero.y + hero.speed
+    if love.keyboard.isDown("s") and hero.screenY + hero.offSetY < height then
+      hero.screenY = hero.screenY + hero.speed
     end
-    if love.keyboard.isDown("d") and hero.y + hero.offSetY < height then
-      hero.x = hero.x + hero.speed
+    if love.keyboard.isDown("d") and hero.screenX + hero.offSetX< width then
+      hero.screenX = hero.screenX + hero.speed
     end
-    if love.keyboard.isDown("q") and hero.y - hero.offSetY > 0 then
-      hero.x = hero.x - hero.speed
+    if love.keyboard.isDown("q") and hero.screenX - hero.offSetX  > 0 then
+      hero.screenX = hero.screenX - hero.speed
     end
   end
 
 function hero.draw()
-  love.graphics.rectangle("fill", hero.x - hero.offSetX, hero.y - hero.offSetY , hero.width, hero.height)
+  love.graphics.rectangle("fill", hero.screenX - hero.offSetX, hero.screenY - hero.offSetY , hero.width, hero.height)
 
 end
 
