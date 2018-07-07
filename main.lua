22e91965cb7a437d5106810ec5671e1a41a109c0
Sungie <benmanu974@gmail.com>
2018-07-07 15:51:43 +0200

    create temporary hero

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+local hero = {}
+hero.x = 0
+hero.y = 0
+hero.Twidth = 100
+hero.Theight = 100
+hero.speed = 10
+
 function love.load()
 
   largeur = love.graphics.getWidth()
@@ -6,11 +13,22 @@ function love.load()
 end
 
 function love.update(dt)
-
+  if love.keyboard.isDown("z") then
+    hero.y = hero.y - hero.speed
+  end
+  if love.keyboard.isDown("s") then
+    hero.y = hero.y + hero.speed
+  end
+  if love.keyboard.isDown("d") then
+    hero.x = hero.x + hero.speed
+  end
+  if love.keyboard.isDown("q") then
+    hero.x = hero.x - hero.speed
+  end
 end
 
 function love.draw()
-
+  love.graphics.rectangle("fill", hero.x, hero.y, hero.Twidth, hero.Theight)
 end
 
 function love.keypressed(key)
