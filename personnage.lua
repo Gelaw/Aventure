5babaebc8936b1b9f6b74bb32387ab8fb6294a32
Gelaw <Gelaw974@gmail.com>
2018-07-07 22:44:41 +0200

    edited Personnage

--- personnage.lua
+++ personnage.lua
@@ -7,6 +7,41 @@ local Personnage = {}
     personnage.width = 50
     personnage.height = 50
     personnage.speed = 1
+    personnage.timer = 0
+    personnage.isWalkable = {}
+
+    function init(x, y, isWalkable)
+      personnage.x = x
+      personnage.y = y
+      personnage.isWalkable = isWalkable
+    end
+
+    function update(dt)
+      if timer >= dt then
+        timer = timer - dt
+      else
+        timer = 0
+      end
+    end
+
+    function move(direction)
+      local newx = personnage.x
+      local newy = personnage.y
+      if direction == 0 then
+        new.y = new.y - 1
+      elseif direction == 1 then
+        new.x = new.x + 1
+      elseif direction == 2 then
+        new.y = new.y + 1
+      elseif direction == 3 then
+        new.x = new.x - 1
+      end
+      if personnage.isWalkable(newx, newy) then
+        personnage.x = newx
+        personnage.y = newy
+        personnage.timer = speed
+      end
+    end
 
     return personnage
   end
