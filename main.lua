3a38ed698fbbdb77d882a6ef7af907391d3befa3
Sungie <benmanu974@gmail.com>
2018-07-07 15:05:47 +0200

    add main.lua

---
+++ main.lua
@@ -0,0 +1,20 @@
+function love.load()
+
+  largeur = love.graphics.getWidth()
+  hauteur = love.graphics.getHeight()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+
+  print(key)
+
+end
