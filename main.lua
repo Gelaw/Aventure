fdb7f6016b922f0936d41ca170deff5ef64fcd3a
Sungie <benmanu974@gmail.com>
2018-07-08 11:37:16 +0200

    add sleep(n)

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 --additionnal materials
 function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
-
+local clock = os.clock
+function sleep(n)
+  local t0 = clock()
+  while clock() - to <= n do end
+end
 --require
 local room = require("room")
 
